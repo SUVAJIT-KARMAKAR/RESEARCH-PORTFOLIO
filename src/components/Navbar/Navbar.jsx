681791e87ba392843394316b01332ec0ffa44292
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2024-10-30 19:08:06 +0530

    SUVAJITKARMAKAR | >Manoswita | Portfolio | Readme.md file updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
         <header className="navbar">
             <div className="navbar-text"> <h2> MANOSWITA </h2> </div>
             <nav className="navbar-links" ref={navigationRef}>
-                <a href="#experience-header"> EXPERIENCE </a>
-                <a href="#publication-header"> PUBLICATIONS </a>
-                <a href="#"> AWARDS </a>
+                <a href="#experience-header" target="_blank"> EXPERIENCE </a>
+                <a href="#publication-header" target="_blank"> PUBLICATIONS </a>
+                <a href="#" target="_blank"> HONOURS </a>
                 <a href="#" className="cv-button"> DOWNLOAD CV </a>
                 {/* Navigation close button */}
                 <button className="navbar-button navbar-close-button"onClick={showNavigationBar}>
