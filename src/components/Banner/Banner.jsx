910310e9f1bf3b633410eb3e332de881873991cb
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2024-10-30 18:39:53 +0530

    SUVAJITKARMAKAR | >Manoswita | Portfolio | Animations added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
-// src > components > Banner > Banner.jsx 
-import React from "react";
+// src/components/Banner/Banner.jsx
+import React, { useEffect, useRef } from "react";
 import HeaderImage from "../../assets/Header-image.png";
 import { FaGoogleScholar } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
@@ -10,10 +10,33 @@ import { FaInstagram } from "react-icons/fa6";
 import "./Banner.modules.css";
 
 const Banner = () => {
+    const bannerRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    bannerRef.current.classList.add("animate");
+                }
+            },
+            { threshold: 0.3 } // Adjust this threshold to control when the animation starts
+        );
+
+        if (bannerRef.current) {
+            observer.observe(bannerRef.current);
+        }
+
+        return () => {
+            if (bannerRef.current) {
+                observer.unobserve(bannerRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <div className="banner">
+        <div className="banner" ref={bannerRef}>
             <div className="banner-image">
-                <img src={HeaderImage} alt="Profile"/>
+                <img src={HeaderImage} alt="Profile" />
             </div>
             <div className="banner-header">
                 <h1> MANOSWITA BOSE</h1>
@@ -28,14 +51,15 @@ const Banner = () => {
                     When away from my laptop, you'll find me buried in a gripping novel, working on a new write-up, shedding tears over some heartbreaking film, treating myself to hot chocolate, or indulging in a bit of retail therapy.
                 </p>
                 <div className="banner-links">
-                    <a href="#"><FaGoogleScholar size={35}/></a>
-                    <a href="https://github.com/manoswita-bose"><FaGithub size={35}/></a>
-                    <a href="https://www.linkedin.com/in/manoswita-bose-467061244/"><FaLinkedin size={35}/></a>
-                    <a href="https://www.facebook.com/bose.manoswita.2001"><FaFacebookSquare size={35}/></a>
-                    <a href="https://www.instagram.com/bose_manoswita/?igsh=MXhvZXkxMnk2cTMxYg%3D%3D#"><FaInstagram size={35}/></a>
+                    <a href="#"><FaGoogleScholar size={35} /></a>
+                    <a href="https://github.com/manoswita-bose"><FaGithub size={35} /></a>
+                    <a href="https://www.linkedin.com/in/manoswita-bose-467061244/"><FaLinkedin size={35} /></a>
+                    <a href="https://www.facebook.com/bose.manoswita.2001"><FaFacebookSquare size={35} /></a>
+                    <a href="https://www.instagram.com/bose_manoswita/?igsh=MXhvZXkxMnk2cTMxYg%3D%3D#"><FaInstagram size={35} /></a>
                 </div>
             </div>
-    </div>
-    )
+        </div>
+    );
 };
+
 export default Banner;
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -29,28 +29,6 @@ const Experience = () => {
                     <span className="right-container-arrow"></span>
                     </div>
                 </div>
-
-                {/* Experience */}
-                <div className="container left-container">
-                    <LuCalendarClock size={40} className="logo"/>
-                    <div className="text-box">
-                    <h2>Experience1</h2>
-                    <small>2018-2019</small>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente eius alias facilis! Voluptas eaque quae ex veritatis blanditiis explicabo.</p>
-                    <span className="left-container-arrow"></span>
-                    </div>
-                </div>
-
-                {/* Experience */}
-                <div className="container right-container">
-                    <LuCalendarClock size={40} className="logo"/>
-                    <div className="text-box">
-                    <h2>Experience1</h2>
-                    <small>2018-2019</small>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente eius alias facilis! Voluptas eaque quae ex veritatis blanditiis explicabo.</p>
-                    <span className="right-container-arrow"></span>
-                    </div>
-                </div>
             </div>
         </>
     )
